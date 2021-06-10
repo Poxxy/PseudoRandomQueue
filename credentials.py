4e86a32d668f4834309c74f87c4d6a8917cf06c4
Poxxy <41162160+Poxxy@users.noreply.github.com>
2021-06-10 12:05:50 -0400

    Remove APPROVED_MODS
    APPROVED_MODS no longer needed as function ctx.author.is_mod detects mod instead when command is run.

--- credentials.py
+++ credentials.py
@@ -3,5 +3,3 @@ NAME = 'Your/your bot name'
 TWITCH_TOKEN = 'oath:yourtokenhere'
 TWITCH_CLIENT_ID = 'your twitch client id'
 TWITCH_CLIENT_SECRET = 'your twitch client secret'
-
-APPROVED_MODS = ['users','you','trust','to','use','more commands']
